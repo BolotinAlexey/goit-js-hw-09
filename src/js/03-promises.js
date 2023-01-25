74eb3234a2636f3938a64e5cdcd5a7274a08584e
Oleksii Bolotin <samkiral73@gmail.com>
2023-01-25 19:36:26 +0100

    fill createPromise

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -2,7 +2,13 @@ function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
   if (shouldResolve) {
     // Fulfill
+    return Promise.resolve({ position, delay });
   } else {
     // Reject
+    return Promise.reject({ position, delay });
   }
 }
+
+const elForm = document.querySelector('.form');
+
+let timeoutlID = setTimeout(createPromise);
