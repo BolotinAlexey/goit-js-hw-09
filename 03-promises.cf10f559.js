a233df8c842e6883f35505a31b6c50b24cd9f5e0
BolotinAlexey <62134373+BolotinAlexey@users.noreply.github.com>
2023-01-25 18:36:56 +0000

    Deploying to gh-pages from @ BolotinAlexey/goit-js-hw-09@74eb3234a2636f3938a64e5cdcd5a7274a08584e 🚀

---
+++ 03-promises.cf10f559.js
@@ -0,0 +1,2 @@
+document.querySelector(".form");setTimeout((function(e,o){return Math.random()>.3?Promise.resolve({position:e,delay:o}):Promise.reject({position:e,delay:o})}));
+//# sourceMappingURL=03-promises.cf10f559.js.map
