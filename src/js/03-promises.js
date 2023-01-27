b1a8367149477812f38104ce1d806f1116bc08d2
Oleksii Bolotin <samkiral73@gmail.com>
2023-01-27 21:55:57 +0100

    finished task3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,14 +1,48 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-    return Promise.resolve({ position, delay });
-  } else {
-    // Reject
-    return Promise.reject({ position, delay });
+import Notiflix from 'notiflix';
+import 'flatpickr/dist/flatpickr.min.css';
+
+const elForm = document.querySelector('.form');
+elForm.addEventListener('submit', onSubmit);
+
+function onSubmit(e) {
+  e.preventDefault();
+  const { delay, step, amount } = e.target.elements;
+  const obj = {
+    delay: +delay.value,
+    step: +step.value,
+    amount: +amount.value,
+  };
+  if (isCheckNoValidData(obj)) {
+    Notiflix.Notify.failure('Please enter correct data');
+    return;
+  }
+  generatorPromises(obj);
+}
+
+// @return true if not valid @params
+function isCheckNoValidData({ delay, step, amount }) {
+  return delay < 0 || step < 0 || amount < 1 ? true : false;
+}
+
+function generatorPromises({ delay, step, amount }) {
+  for (let index = 0; index < amount; index++) {
+    createPromise(index + 1, delay + step * index)
+      .then(({ position, delay }) => {
+        Notiflix.Notify.success(`Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        Notiflix.Notify.failure(`Rejected promise ${position} in ${delay}ms`);
+      });
   }
 }
 
-const elForm = document.querySelector('.form');
-
-let timeoutlID = setTimeout(createPromise);
+function createPromise(position, delay) {
+  const shouldResolve = Math.random() > 0.3;
+  return new Promise((resolve, reject) =>
+    setTimeout(() => {
+      return shouldResolve
+        ? resolve({ position, delay })
+        : reject({ position, delay });
+    }, delay)
+  );
+}
